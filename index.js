ef08ed65c79c3aaba16c4dff099fc0b149ea4d41
Dakota <djsliter@gmail.com>
2022-06-15 22:20:14 -0400

    Committing

---
+++ index.js
@@ -0,0 +1,71 @@
+// DEPENDENCIES --------------------------------------
+import mongoose from 'mongoose';
+import express from 'express';
+import { Blog } from './modules/blog.mjs';
+
+// APP VARIABLES -------------------------------------
+const app = express();
+const PORT = 3000;
+
+// CONFIG ---------------------------------------------
+
+// Use public directory
+app.use(express.static('public'));
+
+// Use json parser
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// Automatically finds and uses PugJS
+app.set('view engine', 'pug');
+
+// Attempt to connect to mongo through mongoose
+async function connect() {
+    await mongoose.connect(
+        'mongodb+srv://sliterd:badpassword1@cluster0.4g78m.mongodb.net/?retryWrites=true&w=majority'
+        );
+}
+// Catch error if connection to DB fails
+connect().catch( (err) => {
+    console.log(err);
+    process.exit(1);
+})
+
+// ROUTES ---------------------------------------------
+
+// API route to get JSON array of Blog Posts
+app.get('/api/entries/', async (req, res) => {
+    const results = await Blog.find({});
+    res.json(results);   
+});
+
+// Render view of all blog posts using Pug
+app.get('/web/entries', async (req, res) => {
+    const results = await Blog.find({});
+    // Render a template, 'allentries'
+    res.render('allentries', {
+        // Set allentries to the array returned from DB
+        allentries: results
+    });
+});
+// Render newentry form for making new Blog Posts
+app.get('/web/newentry', (req, res) => {
+    res.render('newentry');
+});
+
+// Handle post request to /web/newentry
+app.post('/web/newentry', async (req, res) => {
+    // Save body data to model
+    const post = new Blog(req.body);
+    // try to save to DB
+    post.save();
+    // Upon successful save, redirect to entries
+    res.redirect('/web/entries');
+});
+
+// SERVER ----------------------------------------------
+app.listen(PORT, () => {
+    console.log("Listening on Port 3000....");
+});
